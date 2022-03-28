a0c355157792829de9a485b3aa10a27a901c39cf
Nori <loveandfight2186@yahoo.co.jp>
2022-03-27 19:41:51 -0700

    add status column in users table

--- db/migrate/20220328021305_add_status_to_users.rb
+++ db/migrate/20220328021305_add_status_to_users.rb
@@ -1,5 +1,5 @@
 class AddStatusToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :status, :string, default: "active", after: id, comment: "Account Status"
+    add_column :users, :status, :string, default: "active", after: :id, comment: "Account Status"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_02_223355) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_28_021305) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_03_02_223355) do
     t.string "hobby"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "active"
   end
 
 end
